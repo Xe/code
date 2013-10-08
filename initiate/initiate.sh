647b9a1626074f3f9a82b9ec3892ab31bd2038fa
Sam Dodrill <shadow.h511@gmail.com>
2013-10-08 20:31:20 +0000

    Add package lists for distros

--- initiate/initiate.sh
+++ initiate/initiate.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-scp niichan@volantis.yolo-swag.com:/home/niichan/ssh.tgz
+scp niichan@volantis.yolo-swag.com:/home/niichan/ssh.tgz .
 tar zxf ssh.tgz
 
+# Setup packages
+sudo apk add bash zsh ca-certificates alpine-sdk flex bison
+sudo apt-get install zsh build-essential flex bison
+sudo pacman -Syu zsh git flex bison gcc make autoconf
+
+# Clone dotfiles and dependencies
 mkdir code
 cd code
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
@@ -10,9 +16,6 @@ git clone git@github.com:Niichan/dotfiles
 cd dotfiles
 
 # Install oh my zsh
-sudo apk add bash zsh ca-certificates alpine-sdk
-sudo apt-get install zsh
-
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | bash
 
 function setlink
