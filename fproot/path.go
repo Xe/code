5ff09a68a1f822f2cc3e0013979385f0d64b326f
Sam Dodrill <xena@yolo-swag.com>
2014-06-26 16:51:43 -0700

    fproot: simplify

--- fproot/path.go
+++ fproot/path.go
@@ -5,40 +5,6 @@ import (
 	"strings"
 )
 
-// https://gist.github.com/bemasher/1777766
-
-type Stack struct {
-	top *Element
-	size int
-}
-
-type Element struct {
-	value interface{}
-	next *Element
-}
-
-// Return the stack's length
-func (s *Stack) Len() int {
-	return s.size
-}
-
-// Push a new element onto the stack
-func (s *Stack) Push(value interface{}) {
-	s.top = &Element{value, s.top}
-	s.size++
-}
-
-// Remove the top element from the stack and return it's value
-// If the stack is empty, return nil
-func (s *Stack) Pop() (value interface{}) {
-	if s.size > 0 {
-		value, s.top = s.top.value, s.top.next
-		s.size--
-		return
-	}
-	return nil
-}
-
 func testPath(path string) (bool) {
 	length := len(path)
 
@@ -56,19 +22,19 @@ func testPath(path string) (bool) {
 	}
 
 	split := strings.Split(mypath, "/")[1:] // Uninterested in the first slash
-	loc := new(Stack)
+	loc := 0
 
 	for _, val := range split {
 		if val == "." {
 			continue
 		} else if val == ".." {
-			loc.Pop()
+			loc--
 		} else {
-			loc.Push(val)
+			loc++
 		}
 	}
 
-	if loc.Len() == 0 {
+	if loc == 0 {
 		return true
 	}
 
