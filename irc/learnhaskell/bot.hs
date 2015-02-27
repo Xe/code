9496e94b1da5b5384b53687456a6f30cbb282207
Sam Dodrill <xena@yolo-swag.com>
2015-02-27 08:27:32 -0800

    haskell bot: add uptime command

--- irc/learnhaskell/bot.hs
+++ irc/learnhaskell/bot.hs
@@ -2,6 +2,7 @@ import Data.List
 import Network
 import System.IO
 import System.Exit
+import System.Time
 import Control.Arrow
 import Control.Monad.Reader
 import Control.Exception
@@ -14,7 +15,7 @@ nick   = "tutbot"
 
 -- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
 type Net = ReaderT Bot IO
-data Bot = Bot { socket :: Handle }
+data Bot = Bot { socket :: Handle, starttime :: ClockTime }
 
 -- Set up actions to run on start and end, and run the main loop
 main :: IO ()
@@ -26,9 +27,10 @@ main = bracket connect disconnect loop
 -- Connect to the server and return the initial bot state
 connect :: IO Bot
 connect = notify $ do
+    t <- getClockTime
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    return (Bot h)
+    return (Bot h t)
   where
     notify a = bracket_
         (printf "Connecting to %s ... " server >> hFlush stdout)
@@ -59,6 +61,7 @@ listen h = forever $ do
 -- Dispatch a command
 eval :: String -> Net ()
 eval     "!quit"               = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval    "!uptime"              = uptime >>= privmsg
 eval x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
 eval     _                     = return () -- ignore everything else
 
@@ -73,6 +76,27 @@ write s t = do
     io $ hPrintf h "%s %s\r\n" s t
     io $ printf    "> %s %s\n" s t
 
+--
+-- Pretty print the date in '1d 9h 9m 17s' format
+--
+pretty :: TimeDiff -> String
+pretty td = join . intersperse " " . filter (not . null) . map f $
+    [(years          ,"y") ,(months `mod` 12,"m")
+    ,(days   `mod` 28,"d") ,(hours  `mod` 24,"h")
+    ,(mins   `mod` 60,"m") ,(secs   `mod` 60,"s")]
+  where
+    secs    = abs $ tdSec td  ; mins   = secs   `div` 60
+    hours   = mins   `div` 60 ; days   = hours  `div` 24
+    months  = days   `div` 28 ; years  = months `div` 12
+    f (i,s) | i == 0    = []
+            | otherwise = show i ++ s
+
 -- Convenience.
 io :: IO a -> Net a
 io = liftIO
+
+uptime :: Net String
+uptime = do
+    now  <- io getClockTime
+    zero <- asks starttime
+    return . pretty $ diffClockTimes now zero
