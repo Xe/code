68a21ef8c0c47ca1f654271c441eb6b0fe2d62c5
Christine Dodrill <xena@yolo-swag.com>
2015-05-07 23:35:57 -0700

    make nimbot useful

--- irc/nimbot/bot.nim
+++ irc/nimbot/bot.nim
@@ -1,4 +1,4 @@
-import net
+import message, net
 
 var sock: Socket = newSocket()
 
@@ -9,6 +9,15 @@ sock.send("USER nimbot a a a :bia: a\r\n")
 
 while true:
   var line = TaintedString""
+  var m: message.Message
 
   sock.readLine(line)
-  echo(line)
+  echo(">>> " & line)
+
+  m = line.parseMessage()
+
+  case m.verb
+  of "PING":
+    sock.send("PONG :" & m.args[0] & "\r\n")
+  of "001":
+    sock.send("JOIN #niichan\r\n")
---
+++ irc/nimbot/message.nim
@@ -0,0 +1,1 @@
+../ircmess-nim/message.nim
