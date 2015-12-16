c30400e8422b0e8041f48355f9069fae3935155d
Christine Dodrill <xena@yolo-swag.com>
2015-12-16 12:14:00 -0800

    do the loop in go

--- experiments/go-nim/add.go
+++ experiments/go-nim/add.go
@@ -7,4 +7,11 @@ func add(a, b int) int {
 	return a + b
 }
 
+//export addmanytimes
+func addmanytimes() {
+	for i := 0; i < 100000; i++ {
+		add(i, i)
+	}
+}
+
 func main() {}
--- experiments/go-nim/test.nim
+++ experiments/go-nim/test.nim
@@ -7,10 +7,21 @@ let beginning = cpuTime()
 echo "Starting Go FFI at " & $beginning
 
 for i in countup(1, 100_000):
-  discard libsum.add(i.cint, i.cint)
+  let myi = i.cint
+  discard libsum.add(myi, myi)
 
 let endTime = cpuTime()
 
 echo "Ended at " & $endTime
-
 echo "Total: " & $(endTime - beginning)
+
+let beforeGo = cpuTime()
+
+echo "Doing the entire loop in Go. Starting at " & $beforeGo
+
+libsum.addmanytimes()
+
+let afterGo = cpuTime()
+
+echo "Ended at " & $afterGo
+echo "Total: " & $(afterGo - beforeGo) & " seconds"
